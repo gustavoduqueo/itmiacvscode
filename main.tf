d10c86d619bd729207543f0e1646f8670ba96b04
gustavoduqueo <gustavoduqueo@gmail.com>
2023-06-17 03:32:34 -0500

    DNS settings

--- main.tf
+++ main.tf
@@ -3,6 +3,8 @@
 resource "aws_vpc" "VPCITMIaCVSCode" {
   cidr_block = "${var.vpc_cidr}"
   instance_tenancy = "default"
+  enable_dns_hostnames = true
+  enable_dns_support = true
   tags = {
     Name = "VPCITMIaCVSCode"
   }
